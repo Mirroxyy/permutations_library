cc45bf7ea730c31e4083bee7debc89e94560d23a
fipka <semenov.art20@gmail.com~>
2026-02-11 02:57:35 +0700

    Добавил объявления функций в заголовочном файле

--- include/recursive_algorithms.h
+++ include/recursive_algorithms.h
@@ -6,4 +6,23 @@
 #include <stdint.h>
 #include <string.h>
 
+//Стандартный Backtrack алгоритм
+void permutations_backtrack(int arr[], int n, memory_pool_t* pool, void (*callback)(int[], int))
+
+//Рекурсивный алгоритм перестановки с помощью обменов
+void permutations_recursive_swap(int arr[], int n, void (*callback)(int[], int));
+
+//Рекурсивный алгоритм перестановки в лексиграфическом порядке
+void permutations_recursive_lexicographic(int arr[], int n, memory_pool_t* pool, void (*callback)(int[], int));
+
+//Рекурсивный алгоритм перестановки с ограничениями
+void permutations_with_constraints(int arr[], int n,
+                                    memory_pool_t* pool,
+                                    bool (*constraint)(int[], int, int, void*),
+                                    void* constraint_data,
+                                    void (*callback)(int[], int));
+
+//Рекурсивный алгоритм перестановки с отслеживанием повторения
+void multiset_permutations(int arr[], int n, memory_pool_t* pool, void (*callback)(int[], int));
+
 #endif
