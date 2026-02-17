8c2e227253177d171dfc60c451b0167de1390d03
fipka <semenov.art20@gmail.com~>
2026-02-17 18:18:02 +0700

    gg

--- src/tests/unit_tests.c
+++ src/tests/unit_tests.c
@@ -300,12 +300,6 @@ void test_reverse_narayana() {
 }
 
 
-int arrays_equal(int a[], int b[], int n) {
-    for(int i=0;i<n;i++)
-        if(a[i]!=b[i])
-            return 0;
-    return 1;
-}
 
 void test_factorial_system_functions() {
 
@@ -505,12 +499,6 @@ void test_inversion_functions() {
 }
 
 
-int arrays_equal(int a[], int b[], int n) {
-    for (int i = 0; i < n; i++)
-        if (a[i] != b[i])
-            return 0;
-    return 1;
-}
 
 void test_plus_one_algorithm() {
 
